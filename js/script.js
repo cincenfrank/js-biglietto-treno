3979233c90dfc25553730e2ebc47cea7d13ad07e
cincenfrank <fpetrangeli1@gmail.com>
2021-10-08 15:12:09 +0200

    excercise done

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const priceByKmInEuro = 0.21;
+const juniorDiscount = 0.2;
+const seniorDiscount = 0.4;
+
+let distanceKm = prompt(`Welcome to the best train ticketing service online!
+In order to calculate your ticket we need to know how many kilometers you need to cover for your travel.`);
+
+if (isNaN(parseInt(distanceKm))) {
+  alert(
+    `"${distanceKm}" is a non valid value for distance. Please refresh the page and try again.`
+  );
+} else {
+  let customerAge = prompt(`Well done! 
+Can you please tell us your age in order to verify if we have any type of discount we can apply to your order?`);
+  if (isNaN(parseInt(customerAge))) {
+    alert(
+      `"${customerAge}" is a non valid value for age. Please refresh the page and try again.`
+    );
+  } else {
+    let ticketDiscountPercentage = 0;
+
+    if (customerAge < 18) {
+      ticketDiscountPercentage = juniorDiscount;
+      discountMessage = `Good News! We have applied a ${(
+        juniorDiscount * 100
+      ).toFixed(0)}% discount reserved for our junior customers!`;
+    } else if (customerAge > 65) {
+      ticketDiscountPercentage = seniorDiscount;
+      discountMessage = `Good News! We have applied a ${(
+        seniorDiscount * 100
+      ).toFixed(0)}% discount reserved for our senior customers!`;
+    } else {
+      discountMessage = `We have applied our best offer based on our regular price list.`;
+    }
+
+    let finalMessage = `Thank you for choosing us!`;
+    let ticketPrice = distanceKm * priceByKmInEuro;
+    if (ticketDiscountPercentage > 0) {
+      let discountedPrice = ticketPrice * (1 - ticketDiscountPercentage);
+      finalMessage =
+        `Original price was ${ticketPrice.toFixed(2)}€. You just saved ${(
+          ticketPrice - discountedPrice
+        ).toFixed(2)}€!!!
+` + finalMessage;
+      ticketPrice = discountedPrice;
+    }
+
+    alert(`We have processed your order!
+
+${discountMessage}
+
+Your final ticket price is: ${ticketPrice.toFixed(2)} €.
+
+${finalMessage}`);
+  }
+}
